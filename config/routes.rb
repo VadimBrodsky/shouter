67e04c612e50629510284a5a34f372e55b926d96
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-05-08 22:11:57 -0400

    Add routes for photo shouts

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   resource :dashboard, only: [:show]
   resources :shouts, only: [:show]
   resources :text_shouts, only: [:create]
+  resources :photo_shouts, only: [:create]
 end
