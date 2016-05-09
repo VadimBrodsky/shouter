de44b22d022c52d3426c84396b0847d9251d2eaa
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-05-08 23:00:42 -0400

    Add user associations via following relationsips model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   validates :password_digest, presence: true
 
   has_many :shouts
+  has_many :following_relationships
+  has_many :followed_users, through: :following_relationships
 end
