0c0619b94030933f26509da5ad8fa0d8bc248c00
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-05-08 23:13:48 -0400

    Change foreign key for the users relationships association

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   validates :password_digest, presence: true
 
   has_many :shouts
-  has_many :following_relationships
+  # Override the foreign key from user_id to match the table, with follower_id
+  has_many :following_relationships, foreign_key: :follower_id
   has_many :followed_users, through: :following_relationships
 end
