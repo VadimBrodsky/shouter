4f5d22dee14119079455263154ec8c3786b6e827
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-05-12 06:12:04 -0400

    Add view partial for the timeline, render shouts

--- app/models/timeline.rb
+++ app/models/timeline.rb
@@ -1,12 +1,17 @@
 class Timeline
   # make into an active model compliant object
-  include ActiveModel::Naming
+  # include ActiveModel::Conversion
+  include ActiveModel::Model
 
   def initialize(user)
     @user = user
   end
 
-  # not a concern of the timeline, extend the AC naming instead
+  def shouts
+    @user.shouts
+  end
+
+  # not a concern of the timeline to know template, extend the AC naming instead
   # def to_partial_path
   #   'timelines/timeline'
   # end
