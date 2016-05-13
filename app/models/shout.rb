d84f4d63ffa7f478d3b9d6c3c2a3b7143d11a017
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-05-13 07:00:08 -0400

    Abstract search from Shout model to Search class

--- app/models/shout.rb
+++ app/models/shout.rb
@@ -4,8 +4,5 @@ class Shout < ActiveRecord::Base
 
   default_scope { order('created_at DESC') }
 
-  def self.search(term)
-    text_shouts = TextShout.where("body LIKE ?", "%#{term}%")
-    where(content_type: 'TextShout', content_id: text_shouts)
-  end
+  scope :text_shouts, -> {where(content_type: 'TextShout')}
 end
