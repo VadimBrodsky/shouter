180c9874951f86601dfb05f2ea65c83a361f0fb2
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-05-13 06:19:36 -0400

    Start on making hashtags clickable

--- app/helpers/shouts_helper.rb
+++ app/helpers/shouts_helper.rb
@@ -3,4 +3,14 @@ module ShoutsHelper
     digest = Digest::MD5.hexdigest(user.email)
     image_tag("http://gravatar.com/avatar/#{digest}?s=#{size}")
   end
+
+  def shouterize(text)
+    link_hashtags(text)
+  end
+
+  private
+
+  def link_hashtags(text)
+    text.gsub(/#\w+/) { |match| link_to(match, hashtag_path(match)) }
+  end
 end
